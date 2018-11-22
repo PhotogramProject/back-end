6849efa09da1264e0fbfdee7b0c0fba4eb7b0770
stominmetal <stoyan@astrapaging.com>
2018-11-22 13:27:51 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,3 +1,3 @@
 <?php
-$mysqli = mysqli_connect("localhost", "stomin", "1q2a3z4", "photogram");
+$mysqli = mysqli_connect("sameple_host", "sample_user", "sample_password", "sampla_database");
 $mysqli->set_charset("utf8");
